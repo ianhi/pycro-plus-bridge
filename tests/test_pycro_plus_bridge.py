17b5cefa396db0b901d0fa992ee2f45640a8a57d
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-03-17 14:33:31 -0400

    basic tests

--- tests/test_pycro_plus_bridge.py
+++ tests/test_pycro_plus_bridge.py
@@ -1,2 +1,16 @@
-def test_something():
-    pass
+from pathlib import Path
+
+from pycromanager import start_headless
+from pymmcore_plus import find_micromanager
+
+from pycro_plus_bridge import pycroCorePlus
+
+# Start the Java process
+
+
+def test_for_smoke():
+    mm_app_path = Path(find_micromanager())
+    start_headless(
+        str(mm_app_path), str(mm_app_path / "MMConfig_demo.cfg"), timeout=5000
+    )
+    pycroCorePlus()
